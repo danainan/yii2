77e841e718a8131605e105aa73493329ec77fa14
danainan <danainan555@gmail.com>
2022-09-15 20:40:24 +0700

    new

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -47,21 +47,7 @@ $this->title = 'My Yii Application';
         </div>
     </div>
 
-    <div>
-        <div class="card-group">
-            <div class="card">
-
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 
 </div>
 
